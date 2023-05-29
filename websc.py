fe74d99a0398b4947667591aa068e5fdfaa07a15
Alriosa <carlos.alfredo.rios15@gmail.com>
2023-05-28 22:37:05 -0600

    v1.3
    This reverts commit 8901d58fbd17d21006a0ce0b1a2581fd2a429fcd.

--- websc.py
+++ websc.py
@@ -1,3 +1,4 @@
+import sys
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
@@ -7,11 +8,23 @@ from datetime import datetime
 def descargar_archivo(url):
     nombre_archivo = url.split("/")[-1]  # Obtener el nombre del archivo de la URL
     ruta_destino = r"E:\Emulator\4-Downloads\PS1" + "\\" + nombre_archivo
-    print(f"Descargando: {nombre_archivo}")
-    response = requests.get(url)
+    sys.stdout.write(f"Descargando: {nombre_archivo} ")
+    sys.stdout.flush()
+    response = requests.get(url, stream=True)
+    tamano_total = int(response.headers.get("Content-Length"))
+    tamano_descargado = 0
     with open(ruta_destino, "wb") as archivo:
-        archivo.write(response.content)
-    print(f"{nombre_archivo} descargado correctamente.")
+        for chunk in response.iter_content(chunk_size=8192):
+            if chunk:
+                archivo.write(chunk)
+                tamano_descargado += len(chunk)
+                porcentaje = (tamano_descargado / tamano_total) * 100
+                sys.stdout.write(f"\rProgreso de la descarga: {porcentaje:.2f}% ")
+                sys.stdout.flush()
+    sys.stdout.write("\n")
+    sys.stdout.flush()
+    sys.stdout.write(f"{nombre_archivo} descargado correctamente.\n")
+    sys.stdout.flush()
 
 
 def guardar_enlaces(enlaces):
@@ -20,7 +33,29 @@ def guardar_enlaces(enlaces):
     with open(ruta_archivo, "w") as archivo:
         for enlace in enlaces:
             archivo.write(enlace + "\n")
-    print(f"Archivo de enlaces guardado correctamente: {nombre_archivo}")
+    sys.stdout.write(f"Archivo de enlaces guardado correctamente: {nombre_archivo}\n")
+    sys.stdout.flush()
+
+
+def leer_registro():
+    try:
+        nombre_archivo = "registro_descargas.txt"
+        ruta_archivo = r"E:\Emulator\4-Downloads\PS1" + "\\" + nombre_archivo
+        with open(ruta_archivo, "r") as archivo:
+            enlaces_descargados = archivo.read().splitlines()
+        return enlaces_descargados
+    except FileNotFoundError:
+        return []
+
+
+def guardar_registro(enlaces_descargados):
+    nombre_archivo = "registro_descargas.txt"
+    ruta_archivo = r"E:\Emulator\4-Downloads\PS1" + "\\" + nombre_archivo
+    with open(ruta_archivo, "w") as archivo:
+        for enlace in enlaces_descargados:
+            archivo.write(enlace + "\n")
+    sys.stdout.write("Registro de descargas actualizado.\n")
+    sys.stdout.flush()
 
 
 def analizar_pagina(url, palabras_filtro, limite_descargas, descargar_links):
@@ -31,6 +66,7 @@ def analizar_pagina(url, palabras_filtro, limite_descargas, descargar_links):
     contador_descargas_posibles = 0
     descargas_realizadas = 0
     enlaces_atrapados = []
+    enlaces_descargados = leer_registro()
 
     for enlace in enlaces:
         href = enlace.get("href")
@@ -43,13 +79,22 @@ def analizar_pagina(url, palabras_filtro, limite_descargas, descargar_links):
             if descargar_links:  # Verificar si se deben descargar los enlaces
                 if not descargas_realizadas < limite_descargas:  # Verificar si se ha alcanzado el límite de descargas
                     break
+                if urljoin(url, href) in enlaces_descargados:  # Verificar si el enlace ya ha sido descargado
+                    sys.stdout.write(f"El archivo {href} ya ha sido descargado previamente.\n")
+                    sys.stdout.flush()
+                    continue
                 url_descarga = urljoin(url, href)
-                print(f"Procesando enlace: {url_descarga}")
+                sys.stdout.write(f"Procesando enlace: {url_descarga}\n")
+                sys.stdout.flush()
                 descargar_archivo(url_descarga)
                 descargas_realizadas += 1
+                enlaces_descargados.append(urljoin(url, href))
 
-    print(f"Total de descargas posibles: {contador_descargas_posibles}")
-    print(f"Descargas realizadas: {descargas_realizadas}")
+    guardar_registro(enlaces_descargados)
+
+    sys.stdout.write(f"Total de descargas posibles: {contador_descargas_posibles}\n")
+    sys.stdout.write(f"Descargas realizadas: {descargas_realizadas}\n")
+    sys.stdout.flush()
 
     if not descargar_links and enlaces_atrapados:
         guardar_enlaces(enlaces_atrapados)
@@ -61,7 +106,8 @@ palabras_filtro = ["japan", "europa", "eur", "jap", "(japan)", "(Japan)", "(JAPA
                    "Germany", "France", "FRANCE", "france", "Italy", "ITALY", "(Netherlands)", "netherlands", "Korea",
                    "Scandinavia", "Australia", "(Australia)", "(Sweden)", "(Norway)", "(Denmark)", "(Russia)", "Russia",
                    "(China)", "china", "China", "Taiwan", "taiwan", "(Taiwan)", "portugal", "portuguese", "brazil",
-                   "brasil", "fifa", "wwf", "football", "futbol", "volleyball","Tenis","(asia)","(Asia)","asia","NFL"]  # Palabras filtro
+                   "brasil", "fifa", "wwf", "football", "futbol", "volleyball", "Tenis", "(asia)", "(Asia)", "asia",
+                   "NFL"]
 limite_descargas = 5  # Límite de descargas simultáneas
 descargar_links = False  # Indicador para descargar los enlaces (False para solo analizar)
 analizar_pagina(url_pagina, palabras_filtro, limite_descargas, descargar_links)
